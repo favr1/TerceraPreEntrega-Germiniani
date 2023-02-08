a615e0bd7c9f046b4632c2103ede21ed5daa258f
favr1 <misterdoge05@gmail.com>
2023-02-07 21:58:25 -0300

    otra mod

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -3,8 +3,17 @@ from django.http import HttpResponse
 from AppCoder.models import Curso
 
 # Create your views here.
-def curso(self):
-    curso = Curso(nombre="Desarrollo web", comision=19881)
-    curso.save()
-    documentoDeTexto = f'--> Curso: {curso.nombre} comision: {curso.comision}'
-    return HttpResponse(documentoDeTexto)
+def inicio(request):
+    return HttpResponse('vista inicio')
+
+def cursos(request):
+    return HttpResponse('vista cursos')
+
+def profesores(request):
+    return HttpResponse('vista profesores')
+
+def entregrables(request):
+    return HttpResponse('vista entregables')
+
+def estudiantes(request):
+    return HttpResponse('vista estudiantes')
